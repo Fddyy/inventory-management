823d8f972764a1cac2241b907c82ef098f3178e1
Ferdy <frdytmvn@gmail.com>
2024-12-12 23:27:27 +0800

    menambahkan middleware cors

--- src/server.js
+++ src/server.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views',path.resolve('src/views'));
+app.use(cors(corsOptions))
 
 // Routes
 const homeRoute = require('./routes/indexRoute');
